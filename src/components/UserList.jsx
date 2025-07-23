9ecf4c5df9ce3463eef5cb1f7b83255d02f9e562
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-07-23 14:18:58 +0800

    feat: Add UserList component

---
+++ src/components/UserList.jsx
@@ -0,0 +1,36 @@
+import UserListItem from "./UserListItem";
+
+/**
+ * 
+ * @param {Array} users - List of user objects
+ * @param {function} onUpdateUser - Callback to update the user
+ * @param {function} onDeleteUser - Callback to delete the user
+ * @param {boolean} [disabled=false] - A flag whether the list is disabled
+ * @returns {JSXElement}
+ */
+
+const UserList = ({ users, onUpdateUser, onDeleteUser, disabled = false }) => {
+    if (users.length === 0) {
+        return (
+            <div>
+                No users found. Add a user to get started!
+            </div>
+        );
+    }
+
+    return (
+        <ul>
+            { users.map(user => (
+                <UserListItem
+                    key={user.id}
+                    user={user}
+                    onUpdate={onUpdateUser}
+                    onDelete={onDeleteUser}
+                    disabled={disabled}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default UserList;
