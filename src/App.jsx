abd54af24869b27044de2b0aa3753df33635899d
ReignSans <reignsantos75@gmail.com>
2025-07-24 00:52:03 +0800

    feat: create basic dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandPage from './pages/LandPage';
 import LogInPage from './pages/LogInPage';
 import RegPage from './pages/RegPage';
+import Dashboard from './pages/Dashboard';
 import './App.css'
-
+ 
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<LandPage />} />
         <Route path='/login' element={<LogInPage />} />
         <Route path='/register' element={<RegPage />} />
+        <Route path='/dashboard' element={<Dashboard />}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+
+
+function Dashboard() {
+
+    return (
+        <div className='container'>
+           <h1>Dashboard</h1>
+            <div className='numberRecordBox'>
+                <h3>Number of Records</h3>
+            </div>
+            <div className="latestRecordBox">
+                <h3>Latest Records</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
