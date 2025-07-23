1cef7a0a855a3a7bbdcfa1453fae89ed490b73eb
ABO <140602836+AB0-0@users.noreply.github.com>
2025-07-23 16:38:17 +0800

    add userData.js

---
+++ src/userData.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    name: 'John Doe',
+    studNum: '12345678',
+    address: 'Santo Rosario',
+    HauEmail: 'jdoe@student.hau.edu.ph',
+    password: '123456'
+  },
+  {
+    name: 'Ashlee Jennifer Lee',
+    studNum: '20760570',
+    address: 'San Fernando',
+    HauEmail: 'avlee1@student.hau.edu.ph',
+    password: '654321'
+  },
+];
