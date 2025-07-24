a2eb88483e8d5a4e92f61b5c186043ad0a0bbd39
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-07-24 22:46:05 +0800

    feat: add AddRecordsPage

---
+++ src/pages/AddRecordsPage.jsx
@@ -0,0 +1,11 @@
+import UserForm from "../components/UserForm";
+import Sidebar from "../components/Sidebar";
+
+export default function AddRecordsPage() {
+    return (
+        <>  
+            <Sidebar />
+            <UserForm />
+        </>
+    )
+}
