d666ced2906f58020b3f40b343fa72ab72f8ce28
luizaki <francinelouise.sanchez@gmail.com>
2025-07-25 02:44:39 +0800

    fix: fix user   vverification on login

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import {useNavigate } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+import { users } from '../userData.js';
 
 function LogInPage() {
     const[hauEmail, setHauEmail] = useState('');
@@ -18,15 +18,15 @@ function LogInPage() {
         }
     };
 
-    return(
+    return (
         <div>
             <h1>LOGIN</h1>
             <div className='loginBox'>
-            <input placeholder='HAU Email' onChange={(e)=> setUsername(e.target.value)}/>
-            <br />
-            <input placeholder='Password' onChange={(e)=> setPassword(e.target.value)}/>
-            <br />
-            <button onClick={login}>LOGIN</button>
+                <input placeholder='HAU Email' value={hauEmail} onChange={(e)=> setHauEmail(e.target.value)}/>
+                <br />
+                <input placeholder='Password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                <br />
+                <button onClick={login}>LOGIN</button>
             </div>
         </div>
     );
