81ea8df5b4acbba2d1a0484913b0a2aee92e3a94
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-07-24 15:53:05 +0800

    feat: add id parameter in user object

--- src/userData.js
+++ src/userData.js
@@ -1,5 +1,6 @@
 export const users = [
-  {
+  { 
+    id: 1,
     name: 'John Doe',
     studNum: '12345678',
     address: 'Santo Rosario',
@@ -7,6 +8,7 @@ export const users = [
     password: '123456'
   },
   {
+    id: 2,
     name: 'Ashlee Jennifer Lee',
     studNum: '20760570',
     address: 'San Fernando',
