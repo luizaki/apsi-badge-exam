2e5b560c3cab64b136d2efeae2acdd8ef1c598cb
ReignSans <reignsantos75@gmail.com>
2025-07-26 01:47:43 +0800

    feat: add dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,20 @@
 import { users } from '../userData.js';
 import PageLayout from '../components/PageLayout.jsx';
+import '../styles/Dashboard.css';
 
 function Dashboard() {
 
     return (
         <PageLayout title='Dashboard'>
-            <div className='numberRecordBox'>
-                <h3>Number of Records</h3>
-                <p>{users.length}</p>
-            </div>
-            <div className="latestRecordBox">
-                <h3>Latest Records</h3>
-                <p>{users.at(-1).name}</p>
+            <div className='container'>
+                <div className='numberRecordBox'>
+                    <h3>Number of Records</h3>
+                    <p>{users.length}</p>
+                </div>
+                <div className="latestRecordBox">
+                    <h3>Latest Records</h3>
+                    <p>{users.at(-1).name}</p>
+                </div>
             </div>
         </PageLayout>
     );
