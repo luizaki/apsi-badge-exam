462e4a0e4be38d4d3cebaa1c5d0b4b8b37eb58a0
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-07-26 23:34:46 +0800

    feat: hashed the stored passwords

--- src/userData.js
+++ src/userData.js
@@ -5,7 +5,7 @@ export const users = [
     studNum: '12345678',
     address: 'Santo Rosario',
     HauEmail: 'jdoe@student.hau.edu.ph',
-    password: '123456'
+    password: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'
   },
   {
     id: 2,
@@ -13,6 +13,6 @@ export const users = [
     studNum: '20760570',
     address: 'San Fernando',
     HauEmail: 'avlee1@student.hau.edu.ph',
-    password: '654321'
+    password: '481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5'
   },
 ];
