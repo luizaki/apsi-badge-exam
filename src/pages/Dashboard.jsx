99dcad695f2f3c60f341b290e90523780faf255f
luizaki <francinelouise.sanchez@gmail.com>
2025-07-29 00:43:28 +0800

    refactor: use authentication check from pagelayout to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,14 @@
-import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '../providers/UserProvider.jsx';
 import PageLayout from '../components/PageLayout.jsx';
 import '../styles/Dashboard.css';
 
 function Dashboard() {
-    const { users, isAuthenticated } = useUserContext();
-    const navi = useNavigate();
+    const { users } = useUserContext();
 
     return (
         <>
-            { isAuthenticated 
-            ? (
-                <PageLayout title='Dashboard'>
-                    <div className='container'>
+            <PageLayout title={'Dashboard'}>
+                <div className='container'>
                         <div className='numberRecordBox'>
                             <h3>Number of Records</h3>
                             <p>{users.length}</p>
@@ -21,14 +17,8 @@ function Dashboard() {
                             <h3>Latest Records</h3>
                             <p>{users.at(-1).name}</p>
                         </div>
-                    </div>
-                </PageLayout>
-            ) : (
-                <>
-                    <h1 className='error-page'>Error 403. User is not authenticated.</h1>
-                    <button className='go-back' onClick={() => navi('/login')}>Go back to login</button> 
-                </>
-            ) }
+                </div>
+            </PageLayout>
         </>
 
 
