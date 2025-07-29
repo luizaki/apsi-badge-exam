58f814ba7bcc2cfae7c0b9f243f5322115dc3866
luizaki <francinelouise.sanchez@gmail.com>
2025-07-29 14:09:09 +0800

    fix: fix email validation on updating records

--- src/components/UserListItem.jsx
+++ src/components/UserListItem.jsx
@@ -29,6 +29,12 @@ const UserListItem = ({ user, onUpdate, onDelete, disabled = false}) => {
     };
 
     const handleSaveEdit = async () => {
+        // add regex match to validate editEmail
+        if (!editEmail.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+
         if (!editName?.trim() || !editEmail?.trim() || editName === user.name && editEmail === user.HauEmail) {
             handleCancelEdit();
             return;
