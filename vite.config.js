ab0727bcca2e6925da2f82144e6d04eac7aa7a19
luizaki <francinelouise.sanchez@gmail.com>
2025-07-29 14:14:56 +0800

    fix: fix deployed site url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/apsi-badge-exam",
+  base: "/apsi-badge-exam/",
   plugins: [react()],
 })
